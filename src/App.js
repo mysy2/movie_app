e31b504851f8443ee67b470d9295b5e6046650fb
mysy2 <nobalp@gmail.com>
2020-08-20 14:58:39 +0900

    0820

--- src/App.js
+++ src/App.js
@@ -3,50 +3,43 @@ import "./App.css";
 import Movie from "./Movie";
 
 export default class App extends Component {
-	state = {
-		movies: [
-			{
-				title: "극한직업",
-				poster: "https://upload.wikimedia.org/wikipedia/ko/c/cf/%EA%B7%B9%ED%95%9C%EC%A7%81%EC%97%85_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg"
-			},
-			{
-				title: "라라랜드",
-				poster: "https://upload.wikimedia.org/wikipedia/ko/thumb/1/12/La_la_land.jpg/220px-La_la_land.jpg"
-			},
-			{
-				title: "신세계",
-				poster: "https://upload.wikimedia.org/wikipedia/ko/6/6b/%EC%98%81%ED%99%94_%EC%8B%A0%EC%84%B8%EA%B3%84_%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg"
-			},
-			{
-				title: "매드맥스",
-				poster:
-					"https://upload.wikimedia.org/wikipedia/ko/thumb/1/13/%EB%A7%A4%EB%93%9C%EB%A7%A5%EC%8A%A4_%EB%B6%84%EB%85%B8%EC%9D%98_%EB%8F%84%EB%A1%9C.jpg/220px-%EB%A7%A4%EB%93%9C%EB%A7%A5%EC%8A%A4_%EB%B6%84%EB%85%B8%EC%9D%98_%EB%8F%84%EB%A1%9C.jpg"
-			}
-		]
-	};
+	state = {};
 
 	componentDidMount() {
-		setTimeout(() => {
-			this.setState({
-				movies: [
-					...this.state.movies,
-					{
-						title: "올드보이",
-						poster: "https://upload.wikimedia.org/wikipedia/ko/4/48/Old_Boy.jpg"
-					}
-				]
-			});
-		}, 2000);
+		this._getMovies();
 	}
 
+	_getMovies = async () => {
+		const movies = await this._callApi(); // callApi 작업이 완료되고 return 하기를 await
+		this.setState({
+			movies // movies: movies
+		});
+	};
+
+	_callApi = () => {
+		return fetch("https://yts.mx/api/v2/list_movies.json?sort_by=download_count")
+			.then((response) => response.json())
+			.then((json) => json.data.movies) // arrow 함수는 return 내장
+			.catch((err) => console.log(err));
+	};
+
+	_renderMovies = () => {
+		const movies = this.state.movies.map((movie) => {
+			return (
+				<Movie
+					key={movie.id}
+					title={movie.title_english}
+					poster={movie.medium_cover_image}
+					genres={movie.genres}
+					synopsis={movie.synopsis}
+				/>
+			);
+		});
+		return movies;
+	};
+
 	render() {
-		return (
-			<div className="App">
-				{this.state.greeting}
-				{this.state.movies.map((movie, index) => {
-					return <Movie key={index} title={movie.title} poster={movie.poster} />;
-				})}
-			</div>
-		);
+		const { movies } = this.state;
+		return <div className={movies ? "App" : "App-loading"}>{movies ? this._renderMovies() : "Loading"}</div>;
 	}
 }
